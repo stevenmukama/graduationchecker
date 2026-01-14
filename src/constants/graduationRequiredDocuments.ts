5f875e4496a414204ae2ea3cb9c71a997aa2ae27
mukama steven <stevenmukama1@gmail.com>
2026-01-14 11:35:19 +0200

    remove unnecessary code

--- src/constants/graduationRequiredDocuments.ts
+++ src/constants/graduationRequiredDocuments.ts
@@ -1,7 +1,7 @@
 export const GRADUATION_REQUIRED_DOCUMENTS = [
-  "Academic Transcript",
-  "Thesis / Final Project Report",
-  "Clearance Form / No Dues Certificate",
-  "Identity / National ID Copy",
-  "Graduation Application Form",
+  'Academic Transcript',
+  'Thesis / Final Project Report',
+  'Clearance Form',
+  'Identity / National ID Copy',
+  'Graduation Application Form',
 ];
